74486366c780267658e7ef230af3dc9512927dc8
Israa27 <israa.adnan.1997@gmail.com>
2021-09-29 06:52:11 +0300

    upload solution task

--- task.js
+++ task.js
@@ -1,21 +1,51 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+var fs = require('fs');
+//read the data from text file and store in variable
+var numbers = fs.readFileSync('data.txt',"utf-8").split(',');
 
-console.log("Original numbers list: ", numbers)
+//create a file to store the output of sort array numbers
+fs.writeFile('./output.txt','Output of Array', err => {
+  if (err) {console.error(err) 
+    return
+  }
+
+});
+//Show numbers before arranging
+fs.appendFile('./output.txt',`${'\n'}${'Original numbers list:'}${numbers.toString()}`, err => {
+    if (err) {console.error(err) 
+      return
+    }
+  
+  });
 
 for(let i = 0 ; i < numbers.length ; i++)
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+    for(let j = 0 ; j < numbers.length -1 ; j++)
     {
-        if(numbers[i] > numbers[j + 1])
+        //Compare the numbers in the inner loop
+        if(numbers[j] > numbers[j + 1])
+        {
+            let temp = numbers[j];
+            numbers[j] = numbers[j + 1]
+            numbers[j + 1] = temp;
+
+        }
+    }
+}
+
+//Show numbers in ascending order
+fs.appendFile('./output.txt',`${'\n'}${'Numbers list After sorting:'}${numbers.toString()}`, err => {
+    if (err) {console.error(err) 
+      return
+    }
+  
+  });
+
+for(let i = 0 ; i < numbers.length ; i++)
+{
+    for(let j = 0 ; j < numbers.length -1 ; j++)
+    {
+        //Compare the numbers in the inner loop
+        if(numbers[j] < numbers[j + 1])
         {
             let temp = numbers[j];
             numbers[j] = numbers[j + 1]
@@ -23,22 +53,11 @@ for(let i = 0 ; i < numbers.length ; i++)
         }
     }
 }
-
-console.log("Numbers list After sorting: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
+//Show numbers in descending order
+fs.appendFile('./output.txt',`${'\n'}${'Numbers list After Desc sorting:'}${numbers.toString()}`, err => {
+    if (err) {console.error(err) 
+      return
     }
-}
-
-console.log("Numbers list After Desc sorting: ", numbers)
-
+  
+  });
 
